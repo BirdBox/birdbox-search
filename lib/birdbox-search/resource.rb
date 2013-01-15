f1f768c55c472af10c76292ab9a8ace146aa9168
Matthias Eder <meder@birdbox.com>
2013-01-15 13:35:21 -0700

    Adding additional attributes to the resource model.

--- lib/birdbox-search/resource.rb
+++ lib/birdbox-search/resource.rb
@@ -7,12 +7,19 @@ module Birdbox
       index_prefix ""
       index_name "resources"
 
-      property :title,   :type => 'string', :analyzer => 'snowball'
-      property :url,     :type => 'string', :index => 'not_indexed' 
-      property :type,    :type => 'string', :analyzer => 'keyword'
-      property :tags,    :default => [ ], :index => 'keyword'
-      #property :height, :type => 'number', :index => 'not_indexed'
-      #property :width,  :type => 'number', :index => 'not_indexed'
+      validates_presence_of :source, :owner, :type, :url
+      #validates_format_of :height, :allow_nil => true, :with => /\d+/
+      #validates_format_of :width, :allow_nil => true, :with => /\d+/
+
+      property :source,     :type => 'string', :analyzer => 'keyword'
+      property :owner,      :type => 'string', :analyzer => 'keyword'
+      property :title,      :type => 'string', :analyzer => 'snowball'
+      property :url,        :type => 'string', :index => 'not_indexed' 
+      property :type,       :type => 'string', :analyzer => 'keyword'
+      property :tags,       :default => [ ], :index => 'keyword'
+      property :height,     :type => 'integer', :index => 'not_indexed'
+      property :width,      :type => 'integer', :index => 'not_indexed'
+      property :created_at, :type => 'date'
 
     end
 
