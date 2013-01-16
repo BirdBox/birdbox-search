f4f73f50988ed6adb227841ea489799d768bec8f
Matthias Eder <meder@birdbox.com>
2013-01-16 12:49:57 -0700

    Fixed mapping issue that was causing hyphenated tags to get tokenized.

--- lib/birdbox-search/resource.rb
+++ lib/birdbox-search/resource.rb
@@ -1,35 +1,42 @@
 module Birdbox
   module Search
 
+    # use this for resource id :id = "#{provider}:#{external_id}"
     class Resource
       include Tire::Model::Persistence
+      include Tire::Model::Search
+      include Tire::Model::Callbacks
 
       index_prefix ""
       index_name "resources"
+      document_type "resource"
 
-      validates_presence_of :source, :owner, :type, :url
+      # NOTE: mappings are only applied when calling Resource.create_elasticsearch_index.  Using
+      # Resource.index.create will create a generic index with no mappings.
+      mapping do
+        property :provider,         :type => 'string',  :index => 'not_analyzed'
+        property :owner_uid,        :type => 'string',  :index => 'not_analyzed'
+        property :owner_nickname,   :type => 'string',  :index => 'not_analyzed'
+        property :title,            :type => 'string',  :index => 'analyzed',     :analyzer => 'standard'
+        property :url,              :type => 'string',  :index => 'not_analyzed'
+        property :type,             :type => 'string',  :index => 'not_analyzed'
+        property :tags,             :type => 'string',  :index => 'analyzed',     :analyzer => 'keyword', :default => [ ]
+        property :height,           :type => 'integer', :index => 'no'
+        property :width,            :type => 'integer', :index => 'no'
+        property :created_at,       :type => 'date',    :index => 'not_analyzed'
+        property :taken_at,         :type => 'date',    :index => 'not_analyzed'
+        property :description,      :type => 'string',  :index => 'analyzed',     :analyzer => 'standard'
+        property :thumbnail_url,    :type => 'string',  :index => 'no'
+        property :thumbnail_height, :type => 'integer', :index => 'no'
+        property :thumbnail_width,  :type => 'integer', :index => 'no'
+        property :html,             :type => 'string',  :index => 'no'
+        property :checksum,         :type => 'string',  :index => 'no'
+      end
+
+      #validates_presence_of :source, :owner, :type, :url
       #validates_format_of :height, :allow_nil => true, :with => /\d+/
       #validates_format_of :width, :allow_nil => true, :with => /\d+/
-      
-      # use this for resource id :id = "#{provider}:#{external_id}"
 
-      property :provider,         :type => 'string', :analyzer => 'keyword'
-      property :owner_uid,        :type => 'string', :analyzer => 'keyword'
-      property :owner_nickname,   :type => 'string', :analyzer => 'keyword'
-      property :title,            :type => 'string', :analyzer => 'snowball'
-      property :url,              :type => 'string', :index => 'not_indexed'
-      property :type,             :type => 'string', :analyzer => 'keyword'
-      property :tags,             :default => [ ], :index => 'keyword'
-      property :height,           :type => 'integer', :index => 'not_indexed'
-      property :width,            :type => 'integer', :index => 'not_indexed'
-      property :created_at,       :type => 'date'
-      property :taken_at,         :type => 'date'
-      property :description,      :type => 'string', :analyzer => 'snowball'
-      property :thumbnail_url,    :type => 'string', :index => 'not_indexed'
-      property :thumbnail_height, :type => 'integer', :index => 'not_indexed'
-      property :thumbnail_width,  :type => 'integer', :index => 'not_indexed'
-      property :html,             :type => 'string', :index => 'not_indexed'
-      property :checksum,         :type => 'string', :index => 'not_indexed'
     end
 
   end
--- test/lib/resource_test.rb
+++ test/lib/resource_test.rb
@@ -5,11 +5,22 @@ describe Birdbox::Search::Resource do
 
   before do
     subject.index.delete
+    subject.create_elasticsearch_index
     @items = [ 
-      subject.new(:id => 1, :title => "Purple sunset", :type => "photo", :url => "http://www.example.com/foo.jpg", :tags => %w(birdbox one)),
-      subject.new(:id => 2, :title => "No stone left unturned", :type => "photo", :url => "http://www.example.com/bar.jpg", :tags => %w(birdbox two)),
-      subject.new(:id => 3, :title => "That looks delicious", :type => "photo", :url => "http://www.example.com/baz.jpg", :tags => %w(birdbox three)),
-      subject.new(:id => 4, :title => "Tags good", :type => "photo", :url => "http://www.example.com/biz.jpg", :tags => %w(birdbox-tokenizer three))
+      subject.new(:id => 'fb:1', :provider => "facebook", :owner_uid => "123456", 
+        :owner_nickname => "me", :title => "Purple sunset", :type => "photo", 
+        :description => "A purple sunset off the coast of Isla Vista, CA",
+        :url => "http://www.example.com/foo.jpg", :tags => %w(birdbox one),
+        :height => 640, :width => 480, :created_at => Time.now.utc),
+
+      subject.new(:id => 'fb:2', :title => "No stone left unturned", :type => "photo",
+        :url => "http://www.example.com/bar.jpg", :tags => %w(birdbox two)),
+
+      subject.new(:id => 'fb:3', :title => "That looks delicious", :type => "photo",
+        :url => "http://www.example.com/baz.jpg", :tags => %w(birdbox three)),
+
+      subject.new(:id => 'fb:4', :title => "Tags good", :type => "photo", 
+        :url => "http://www.example.com/biz.jpg", :tags => ['birdbox-tokenizer', 'three'])
     ]
     subject.index.import(@items)
     subject.index.refresh
@@ -22,12 +33,12 @@ describe Birdbox::Search::Resource do
   end
 
   it "must be retrievable by id" do
-    r = subject.find(1)
+    r = subject.find('fb:1')
     r.url.must_equal(@items.first.url) 
   end
 
   it "must be able to retrieve multiple ids" do
-    r = subject.find([2, 3])
+    r = subject.find(['fb:1', 'fb:2'])
     r.size.must_equal(2)
     r.first.url.must_equal(@items[1].url)
   end
@@ -43,18 +54,28 @@ describe Birdbox::Search::Resource do
         query.match :tags, "three"
       }
     }
-    puts r.inspect
     r.size.must_equal(@items.count { |x| x.tags.include?("three") })
   end
   
-  it "must not be tokenized on tags" do
+  it "must not analyze/tokenize tags" do
     r = subject.search { |search|
       search.query { |query|
-        query.match :tags, "birdbox"
+        query.term :tags, "birdbox"
+        #query.string "tags:\"birdbox\""
+        #query.terms :tags, ["birdbox"]
+        #query.boolean { must { term :tags, "birdbox" }}
       }
     }
-    puts r.inspect
     r.size.must_equal(@items.count { |x| x.tags.include?("birdbox") })
+  end
+
+  it "must be able to search for multiple tags" do
+    r = subject.search { |search|
+      search.query { |query|
+        query.terms :tags, ["one", "two"]
+      }
+    }
+    r.size.must_equal(@items.count { |x| x.tags.include?("one") or x.tags.include?("two") })
   end
 
 end
