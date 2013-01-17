760b79eb0ea83cf6fd7371354783491342fce5ad
Matthias Eder <meder@birdbox.com>
2013-01-17 12:26:03 -0700

    Added the ability to fetch nests/

--- lib/birdbox-search/nest.rb
+++ lib/birdbox-search/nest.rb
@@ -2,27 +2,47 @@ module Birdbox
   module Search
 
     class Nest
-      attr_accessor :members, :tags
 
-      def initialize(members, tags)
-        @members = members || { }
-        @tags = tags || [ ]
-      end
-
-
-      def fetch(options={ })
+      def self.fetch(owners, tags, options = { })
         opts = {
-          :count => 20,
-          :since => nil,
-          :until => nil 
+          :count => 20,   # default to returning 20 items
+          :since => nil,  # default to the beginning of time
+          :until => nil   # default to the end of time
         }.merge(options)
 
-        search = Tire.search('resources') { |search|
+        tq = tags.map { |t| "tags:\"#{t}\"" }.join(" OR ") 
+        mq = owners.map { |(k,v)| "(provider:\"#{k}\" AND (#{v.map{ |owner| "owner_uid:\"#{owner}\"" }.join(" OR ")}))" }.join(" OR ")
+        q = "#{tq} AND (#{mq})"
+
+        search = Tire.search(Birdbox::Search::Resource.index_name) { |search|
           search.query { |query|
-            query.terms :tags, self.tags
+            query.string q
           }
         }
         search.results    
+      end
+
+      attr_accessor :owners, :tags
+
+      # Creates a new instance of a Nest object.
+      #
+      # @param [Hash] owners keys are made up of the provider names and the values
+      #   is an array of user ids specific to that provider.
+      # @param [Array] tags an array of string tokens representing tags.
+      # @return [Nest] a nest object
+      def initialize(owners, tags)
+        @owners = owners || { }
+        @tags = tags || [ ]
+      end
+
+      # Fetches all resources associated with a nest.  The association is determined
+      # through a combination of a resources tag(s) and the resources owner on a
+      # respective provider (e.g. Facebook, Twitter, etc.).
+      # 
+      # @param [Hash] options a hash of optional parameters.
+      # @return [Tire::Results::Collection] an iterable collection of results
+      def fetch(options={ })
+        Nest.fetch(self.owners, self.tags)
       end
 
     end
--- test/lib/nest_test.rb
+++ test/lib/nest_test.rb
@@ -9,21 +9,33 @@ describe Birdbox::Search::Nest do
     Resource.create_elasticsearch_index
     @items = [ 
       Resource.new(:id => 'facebook:1', :provider => "facebook", :external_id => "1",
-        :owner_uid => "123456", :owner_nickname => "me", :title => "Purple sunset",
+        :owner_uid => "100001", :owner_nickname => "alice", :title => "Purple sunset",
         :type => "photo", :description => "A purple sunset off the coast of Isla Vista, CA",
-        :url => "http://www.example.com/foo.jpg", :tags => %w(california),
+        :url => "http://www.example.com/isla_vista.jpg", :tags => %w(california),
         :height => 640, :width => 480, :created_at => Time.now.utc),
 
-      Resource.new(:id => 'twitter:1', :provider => "facebook", :external_id => "1",
-        :owner_uid => "123456", :owner_nickname => "me", :title => "That looks delicious",
-        :type => "photo", :description => "That's the best looking cheesebuger I've seen in quite a while",
-        :url => "http://www.example.com/bar.jpg", :tags => %w(cheeseburger),
+      Resource.new(:id => 'facebook:2', :provider => "facebook", :external_id => "2",
+        :owner_uid => "100001", :owner_nickname => "alice", :title => "Bidwell Park",
+        :type => "photo", :description => "Enjoying a long hike in Bidwell Park.",
+        :url => "http://www.example.com/bidwell.jpg", :tags => %w(california),
         :height => 640, :width => 480, :created_at => Time.now.utc),
 
       Resource.new(:id => 'facebook:3', :provider => "facebook", :external_id => "3",
-        :owner_uid => "654321", :owner_nickname => "notme", :title => "The Golden Gate",
+        :owner_uid => "100001", :owner_nickname => "alice", :title => "That looks delicious",
+        :type => "photo", :description => "That's the best looking cheesebuger I've seen in quite a while",
+        :url => "http://www.example.com/cheeseburger.jpg", :tags => %w(cheeseburger),
+        :height => 640, :width => 480, :created_at => Time.now.utc),
+
+      Resource.new(:id => 'twitter:1', :provider => "twitter", :external_id => "1",
+        :owner_uid => "200001", :owner_nickname => "alice", :title => "The Golden Gate",
         :type => "photo", :description => "Look at that, not a cloud in the sky.",
-        :url => "http://www.example.com/baz.jpg", :tags => %w(california),
+        :url => "http://www.example.com/golden_gate.jpg", :tags => %w(california),
+        :height => 640, :width => 480, :created_at => Time.now.utc),
+
+      Resource.new(:id => 'twitter:2', :provider => "twitter", :external_id => "2",
+        :owner_uid => "200002", :owner_nickname => "bob", :title => "The Golden Gate",
+        :type => "photo", :description => "Look at that, not a cloud in the sky.",
+        :url => "http://www.example.com/golden_gate.jpg", :tags => %w(california),
         :height => 640, :width => 480, :created_at => Time.now.utc),
     ]
     Resource.index.import(@items)
@@ -31,11 +43,11 @@ describe Birdbox::Search::Nest do
   end
   
   it "must be able to retrieve the resources belonging to a nest" do
-    members = { :facebook => ['123456'], :twitter => ['999999' ] }
+    members = { :facebook => ['100001'], :twitter => ['200001', '200002'] }
     tags = %w(california)
     nest = Nest.new(members, tags)
     results = nest.fetch
-    puts results.count
+    results.count.must_equal(4)
   end
 
 end
