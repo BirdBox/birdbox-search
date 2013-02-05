2c02e26df0dd72e5f468925fcfc5a6a456543919
Matthias Eder <meder@birdbox.com>
2013-02-05 14:44:24 -0700

    Adding support to limit results to resources contained in specific albums.

--- test/lib/nest_test.rb
+++ test/lib/nest_test.rb
@@ -19,53 +19,63 @@ describe Birdbox::Search::Nest do
   end
   
   it "must be able to fetch resources for a single tag and a single owner" do
-    results = Nest.fetch({:facebook => ['100001']}, ['california'])
+    results = Nest.fetch({:facebook => ['100001']}, ['california'], nil)
     results.count.must_equal(1)
   end
 
   it "must be able to fetch resources for a single tag and multiple owners" do
-    results = Nest.fetch({:facebook => ['100001', '100002'], :twitter => ['200001']}, ['california'])
+    results = Nest.fetch({:facebook => ['100001', '100002'], :twitter => ['200001']}, ['california'], nil)
     results.count.must_equal(3)
   end
 
   it "must be able to fetch resources for multiple tags" do
-    results = Nest.fetch({:facebook => ['100001', '100002']}, %w(cheeseburger california))
+    results = Nest.fetch({:facebook => ['100001', '100002']}, %w(cheeseburger california), nil)
     results.count.must_equal(3)
   end
 
   it "must be able to paginate results" do
     members = { :facebook => ['100001', '100002'], :twitter => ['200001', '200002'] }
     tags = %w(california cheeseburger)
-    results = Nest.fetch(members, tags, :page => 1, :page_size => 5)
+    results = Nest.fetch(members, tags, nil, :page => 1, :page_size => 5)
     results.count.must_equal(5)
-    results = Nest.fetch(members, tags, :page => 2, :page_size => 5)
+    results = Nest.fetch(members, tags, nil, :page => 2, :page_size => 5)
     results.count.must_equal(1)
   end
 
   it "must be able to sort results" do
     members = { :facebook => ['100001', '100002'], :twitter => ['200001', '200002'] }
     tags = %w(california cheeseburger)
-    ascending = Nest.fetch(members, tags, :sort_by => :uploaded_at, :sort_direction => 'asc').map { |r| r.created_at }
-    descending = Nest.fetch(members, tags, :sort_by => :uploaded_at, :sort_direction => 'desc').map { |r| r.created_at }
+    ascending = Nest.fetch(members, tags, nil, :sort_by => :uploaded_at, :sort_direction => 'asc').map { |r| r.created_at }
+    descending = Nest.fetch(members, tags, nil, :sort_by => :uploaded_at, :sort_direction => 'desc').map { |r| r.created_at }
     descending.to_a.must_equal(ascending.to_a.reverse)
   end
 
   it "must support time-bounded queries" do
     members = { :facebook => ['100001', '100002'], :twitter => ['200001', '200002'] }
     tags = %w(california cheeseburger)
-    results = Nest.fetch(members, tags, :since => Time.parse("2013-01-01 00:00:00").to_i)
+    results = Nest.fetch(members, tags, nil, :since => Time.parse("2013-01-01 00:00:00").to_i)
     results.count.must_equal(4)
-    results = Nest.fetch(members, tags, :until => Time.parse("2012-12-31 23:59:59").to_i)
+    results = Nest.fetch(members, tags, nil, :until => Time.parse("2012-12-31 23:59:59").to_i)
     results.count.must_equal(2)
-    results = Nest.fetch(members, tags, :since => Time.parse("2012-12-01 00:00:00").to_i, :until => Time.parse("2012-12-31 23:59:59").to_i)
+    results = Nest.fetch(members, tags, nil, :since => Time.parse("2012-12-01 00:00:00").to_i, :until => Time.parse("2012-12-31 23:59:59").to_i)
     results.count.must_equal(1)
   end
 
   it "must be able to find all people tagged in the resources belonging to a nest" do
-    people = Nest.find_tagged_people({:facebook => ['100001', '100002']}, ['california'])
+    people = Nest.find_tagged_people({:facebook => ['100001', '100002']}, ['california'], nil)
     people.count.must_equal(3)
     people[0].first.must_equal('facebook:000003')
     people[0].last.must_equal(2)
+  end
+
+  it "must be able to fetch resources for a single owner and multiple albums" do
+    results = Nest.fetch({:facebook => ['100001']}, nil, {:facebook => ['1', '2']})
+    results.count.must_equal(3)
+  end
+
+  it "must be able to fetch resources for multiple owners and albums" do
+    results = Nest.fetch({:facebook => ['100001', '100002']}, nil, {:facebook => ['2', '3']})
+    results.count.must_equal(2)
   end
 
 end
